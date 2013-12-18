a73a28f74c1570991a0ad3598856d20d3417e3d3
Ming Ki Chong <unexpected.exception@gmail.com>
2013-12-18 12:15:03 +0000

    Revert 66d8ba4..b1b4562
    This rolls back to commit 66d8ba4989b71432595b12673c45755ec2455065.

--- DBSDiaryJava/src/dbsDiaryDataInterpreter/Driver.java
+++ DBSDiaryJava/src/dbsDiaryDataInterpreter/Driver.java
@@ -3,7 +3,6 @@ package dbsDiaryDataInterpreter;
 public class Driver {
 	  public static void main(String[] args) {
 //		  new DiaryDataInterpreter().start();
-//		  new BloboEventDataInterpreter().start();
-		  new SelectorFrame().setVisible(true);
+		  new BloboEventDataInterpreter().start();
 	  }
 }
